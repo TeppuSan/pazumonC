a877eea7a262e8ecf9eeb5f67344ac10bb7e8d01
TeppuSan <suraimu0206@icloud.com>
2025-04-30 17:44:25 +0900

    ヒットアンドブロー

--- R_7_4.c
+++ R_7_4.c
@@ -9,47 +9,78 @@ typedef char String[1024];
 int main()
 {
     String set;
-    int b = 0;
     printf("***数あてゲーム(レベル2)***\n3桁の数字を当ててください\nただし各桁の数字は重複しません\n");
     srand((unsigned)time(NULL));
-    int answer[3];
+    int size = 3;
+    int input[size];
+    int answer[size];
+    int contenue;
     int t = 0;
     int in = 0;
+    int hit = 0;
+    int blow = 0;
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int n = 0; n < i; n++)
+        do
         {
-            do
-            {
-                in = 0;
-                t = rand() % 10;
-
+            in = 0;
+            t = rand() % 10;
+            for (int n = 0; n < i; n++)
                 if (answer[n] == t)
                     in = 1;
-                else
-                    answer[i] = t;
+        } while (in);
+
+        answer[i] = t;
+    }
+
+    do
+    {
+        hit = 0;
+        blow = 0;
+        for (int nt = 0; nt < size; nt++)
+        {
+            printf("%d\n", answer[nt]);
+            /* code */
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            scanf("%s", set);
+            input[i] = atoi(set);
+        }
+        for (int i = 0; i < size; i++)
+        {
+            for (int n = 0; n < size; n++)
+            {
+                if (answer[i] == input[n])
+                {
+                    if (i == n)
+                        hit++;
+                    else
+                        blow++;
+                }
 
                 /* code */
-            } while (in);
-
+            }
+        }
+        printf("%dヒット%dブロー\n", hit, blow);
+        if (hit != 3)
+        {
+            printf("続けますか?(0終了: 0以外の数字:続ける)>\n");
+            scanf("%s", set);
+            contenue = atoi(set);
+            if (contenue == 0)
+                return 0;
             /* code */
         }
 
         /* code */
-    }
+    } while (hit != 3);
 
-    for (int nt = 0; nt < 3; nt++)
-    {
-        printf("%d", answer[nt]);
-        /* code */
-    }
-
-    // scanf("%s", set);
-    // b = atoi(set);
     // printf("%d回めであたったよ!\n", i);
 
-    printf("***GAMEOVER***\n");
+    printf("***GAMECLEAR***\n");
 
     return 0;
 }
